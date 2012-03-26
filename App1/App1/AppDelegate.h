a74e7c4cfda5211e4477e6fcaa3bb92a2c71b810
nunoabc <nuno@nmf-net.com>
2012-03-26 04:12:06 +0200

    w11

--- App1/App1/AppDelegate.h
+++ App1/App1/AppDelegate.h
@@ -2,7 +2,7 @@
 //  AppDelegate.h
 //  App1
 //
-//  Created by Nuno Fernandes on 26/03/12. Mac 10
+//  Created by Nuno Fernandes on 26/03/12. Mac 10 w11
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
